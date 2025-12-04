33c5239a56ff9a31b2f7d7c6f66348db5fdd0c22
Artis Gulbis <artisgulbis@hotmail.com>
2025-12-04 20:23:37 +0100

    addded pokedex syncing

--- cmd/sync/main.go
+++ cmd/sync/main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"log"
+	"os"
+	"sync"
+	"time"
 
 	"github.com/ArtisGulbis/pokemon-companion-go-backend/db"
 	"github.com/ArtisGulbis/pokemon-companion-go-backend/pokeapi"
@@ -15,6 +18,7 @@ func main() {
 	flag.Parse()
 
 	// 1. Setup database
+	os.Remove("pokemon.db")
 	database, err := db.New("pokemon.db")
 	if err != nil {
 		log.Fatal(err)
@@ -23,14 +27,34 @@ func main() {
 
 	// 2. Create dependencies
 	client := pokeapi.NewClient("https://pokeapi.co")
-	repo := db.NewPokemonRepository(database)
-	syncer := services.NewPokemonSyncer(client, repo)
+	rateLimiter := time.NewTicker(650 * time.Millisecond)
+	defer rateLimiter.Stop()
+
+	pokemonRepo := db.NewPokemonRepository(database)
+	pokedexRepo := db.NewPokedexRepository(database)
+
+	pokemonSyncer := services.NewPokemonSyncer(client, pokemonRepo, rateLimiter)
+	pokedexSyncer := services.NewPokedexSyncer(client, pokedexRepo, rateLimiter)
+
+	var wg sync.WaitGroup
+
+	startTime := time.Now()
 
 	// 3. Sync Pokemon
 	log.Printf("Syncing %d Pokemon...", *limit)
-	if err := syncer.SyncAll(*limit); err != nil {
-		log.Fatal(err)
-	}
+	wg.Go(func() {
+		if err := pokemonSyncer.SyncAll(*limit); err != nil {
+			log.Fatal(err)
+		}
+	})
 
-	log.Println("Sync complete!")
+	log.Printf("Syncing %d Pokedexes...", *limit)
+	wg.Go(func() {
+		if err := pokedexSyncer.SyncAll(*limit); err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	wg.Wait()
+	log.Printf("Sync complete! Time taken: %v", time.Since(startTime))
 }
